eee2d2c2b9f5ade4ffb4db04f1f5e87dfe425340
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-09 11:00:49 -0800

    Replaced r7rs define-library with chicken-native module
    Code by Wolfgang Corcoran-Mathe

--- srfi-196.scm
+++ srfi-196.scm
@@ -1,8 +1,38 @@
-(import scheme)
-(import (chicken base))
-(import (chicken platform))
-(import (only r7rs define-library))
+(module (srfi 196)
+        (range numeric-range vector-range string-range range-append
+         iota-range range? range=? range-length range-ref range-first
+         range-last subrange range-segment range-split-at range-take
+         range-take-right range-drop range-drop-right range-count
+         range-map->list range-for-each range-fold range-fold-right
+         range-any range-every range-filter->list range-remove->list
+         range-reverse range-map range-map->vector range-filter
+         range-remove range-filter-map range-filter-map->list
+         range-index range-index-right range-take-while
+         range-drop-while range-take-while-right
+         range-drop-while-right vector->range range->string
+         range->list range->generator range->vector)
 
-(register-feature! 'srfi-196)
+  (import (scheme)
+          (chicken base)
+          (only (srfi 1) reduce unfold xcons every concatenate)
+          (only (srfi 133) vector-copy string->vector)
+          (srfi 145))
+  (import (chicken platform))
 
-(include "srfi/196.sld")
+  (register-feature! 'srfi-196)
+
+  ;; "seedless" unfold/tabulate.
+  (define (vector-unfold f len)
+    (let ((res (make-vector len)))
+      (let lp ((i 0))
+        (cond ((= i len) res)
+              (else (vector-set! res i (f i))
+                    (lp (+ i 1)))))))
+
+  (define eof-object
+    (let ((*eof-object* (read (open-input-string ""))))
+      (lambda () *eof-object*)))
+
+  (define exact inexact->exact)
+
+  (include "srfi-196-impl.scm"))
