26d6f1ec91db9374841d05c4e31d2afad865305d
Jeremy Steward <jeremy@thatgeoguy.ca>
2021-03-20 15:55:38 -0600

    Organize module exports into categories
    This makes things slightly easier to read. Iteration could probably be
    broken up further, but for now this is sufficient.

--- srfi-196.scm
+++ srfi-196.scm
@@ -1,19 +1,7 @@
-(module (srfi 196)
-        (range numeric-range vector-range string-range range-append
-         iota-range range? range=? range-length range-ref range-first
-         range-last subrange range-segment range-split-at range-take
-         range-take-right range-drop range-drop-right range-count
-         range-map->list range-for-each range-fold range-fold-right
-         range-any range-every range-filter->list range-remove->list
-         range-reverse range-map range-map->vector range-filter
-         range-remove range-filter-map range-filter-map->list
-         range-index range-index-right range-take-while
-         range-drop-while range-take-while-right
-         range-drop-while-right vector->range range->string
-         range->list range->generator range->vector)
-
+(module (srfi 196) ()
   (import (scheme)
           (chicken base)
+          (chicken module)
           (only (srfi 1) reduce unfold xcons every concatenate)
           (only (srfi 133) vector-copy string->vector)
           (srfi 145))
@@ -34,5 +22,32 @@
       (lambda () *eof-object*)))
 
   (define exact inexact->exact)
+
+  ;; Constructors
+  (export range numeric-range iota-range vector-range string-range
+          range-append)
+
+  ;; Predicates
+  (export range range=?)
+
+  ;; Accessors
+  (export range-length range-ref range-first range-last)
+
+  ;; Iteration
+  (export range-split-at subrange range-segment range-take range-take-right
+          range-drop range-drop-right range-count range-any range-every
+          range-map range-map->list range-map->vector range-for-each
+          range-filter-map range-filter-map->list range-filter
+          range-filter->list range-reverse range-remove range-remove->list
+          range-fold range-fold-right)
+
+  ;; Searching
+  (export range-index range-index-right range-take-while range-take-while-right
+          range-drop-while range-drop-while-right)
+
+  ;; Conversion
+  (export range->list range->vector range->string vector->range
+          range->generator)
+
 
   (include "srfi/196.scm"))
