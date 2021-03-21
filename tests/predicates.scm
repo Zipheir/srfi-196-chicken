8986a4b737465c64e0a339b2ca442bae6ccd8cdd
Jeremy Steward <jeremy@thatgeoguy.ca>
2021-03-21 16:58:48 -0600

    Add predicate-over-whole-range tests
    These are originally from iteration.scm, which is still in the process
    of being refactored locally.

--- tests/predicates.scm
+++ tests/predicates.scm
@@ -21,59 +21,114 @@
 ;;;; Predicates
 
 (test-group "Range predicates"
- (test-group "Span of values in ranges with predicate range=?"
+  (define r (numeric-range 10 30))
 
-  (test-assert "Two ranges with zero length span equivalent values."
-               (range=? eqv? (numeric-range 0 0) (numeric-range 5 5)))
+  (test-group "Span of values in ranges with predicate range=?"
 
-  (test-assert "Two ranges with different lengths span different values."
-               (not (range=? eqv? (numeric-range 0 0) (numeric-range 10 30))))
+    (test-assert "Two ranges with zero length span equivalent values."
+                 (range=? eqv? (numeric-range 0 0) (numeric-range 5 5)))
 
-  (let ((rng (numeric-range 10 30)))
+    (test-assert "Two ranges with different lengths span different values."
+                 (not (range=? eqv? (numeric-range 0 0) r)))
+
     (test-assert "A range spans equivalent values to itself."
-                 (range=? eqv? rng rng)))
+                 (range=? eqv? r r))
 
-  (let ((A (numeric-range 10 30))
-        (B (numeric-range 10 30)))
-    (test-assert "Two numeric ranges, A and B, are not the same range."
-                 (not (eq? A B)))
+    (let ((A (numeric-range 10 30))
+          (B (numeric-range 10 30)))
+      (test-assert "Two numeric ranges, A and B, are not the same range."
+                   (not (eq? A B)))
 
-    (test-assert "Numeric ranges A and B span equivalent values."
-                 (range=? eqv? A B)))
+      (test-assert "Numeric ranges A and B span equivalent values."
+                   (range=? eqv? A B)))
 
-  (test-assert "Numeric ranges with differing lengths span different values"
-               (not (range=? eqv? (numeric-range 10 30) (numeric-range 10 20))))
+    (test-assert "Numeric ranges with differing lengths span different values"
+                 (not (range=? eqv? r (numeric-range 10 20))))
 
-  (let ((A (range 2 zero?))
-        (B (vector-range #(#t #f))))
-    (test-assert "Two boolean ranges, A and B, are not the same range"
-                 (not (eq? A B)))
+    (let ((A (range 2 zero?))
+          (B (vector-range #(#t #f))))
+      (test-assert "Two boolean ranges, A and B, are not the same range"
+                   (not (eq? A B)))
 
-    (test-assert "Two boolean ranges, A and B, were constructed by different procedures."
-                 (not (eq? range vector-range)))
+      (test-assert "Two boolean ranges, A and B, were constructed by different procedures."
+                   (not (eq? range vector-range)))
 
-    (test-assert "Boolean ranges A and B span equivalent values"
-                 (range=? eqv? A B))
+      (test-assert "Boolean ranges A and B span equivalent values"
+                   (range=? eqv? A B))
 
-    (test-assert "Order matters when defining / comparing boolean ranges."
-                 (not (range=? eqv? A (vector-range #(#f #t))))))
+      (test-assert "Order matters when defining / comparing boolean ranges."
+                   (not (range=? eqv? A (vector-range #(#f #t))))))
 
-  (test-assert "Subranges of equivalent length and offset span equivalent values."
-               (range=? eqv?
-                        (numeric-range 10 30)
-                        (subrange (numeric-range 0 50) 10 30)))
-
-  (test-assert "Ranges with differing length span differing values."
-               (not
-                 (range=? eqv?
-                          (range 2 zero?)
-                          (numeric-range 10 30)
-                          (subrange (numeric-range 0 50) 10 30))))
-
-  (test-assert "Ranges with same length but differing offsets span differing values."
-               (not
+    (test-assert "Subranges of equivalent length and offset span equivalent values."
                  (range=? eqv?
                           (numeric-range 10 30)
-                          (subrange (numeric-range 0 50) 11 31))))
+                          (subrange (numeric-range 0 50) 10 30)))
+
+    (test-assert "Ranges with differing length span differing values."
+                 (not
+                   (range=? eqv?
+                            (range 2 zero?)
+                            r
+                            (subrange (numeric-range 0 50) 10 30))))
+
+    (test-assert "Ranges with same length but differing offsets span differing values."
+                 (not
+                   (range=? eqv?
+                            r
+                            (subrange (numeric-range 0 50) 11 31))))
+    )
+
+  (test-group "Predicates over the whole range"
+    (test "Counting over every value in range returns length of range."
+          (range-length r)
+          (range-count (lambda _ #t) r))
+
+    (test "Counting over no value in range returns 0."
+          0
+          (range-count (lambda _ #f) r))
+
+    (test "range-count has similar semantics to counting over a list"
+          (count even? (iota 20 10))
+          (range-count even? r))
+
+    (test "Counting across multiple ranges counts predicate successes."
+          1
+          (range-count (lambda (x y) y)
+                       r
+                       (range 2 zero?)))
+
+    (test "Counting predicates over mapped values."
+          (range-length r)
+          (range-count zero?
+                       (range-map - r r)))
+
+    (test-assert "Searching for any even number in (numeric-range 10 30) succeeds."
+                 (range-any even? r))
+
+    (test-assert "Searching for any always-false predicate fails."
+                 (not (range-any (lambda _ #f) r)))
+
+    (test-assert "Searching across multiple ranges succeeds."
+                 (range-any (lambda (x y) y)
+                            r
+                            (range 2 zero?)))
+
+    (test-assert "Searching for any zero value in mapped numeric range."
+                 (not (range-any zero?
+                                 (range-map + r r))))
+
+    (test-assert "Every value in numeric range is a number."
+                 (range-every number? r))
+
+    (test-assert "Not every value in numeric range is even."
+                 (not (range-every even? test-num-range)))
+
+    (test-assert "Every value tested with a boolean range fails."
+                 (not (range-every (lambda (x y) y)
+                                   r
+                                   (range 2 zero?))))
+
+    (test-assert "Every value from mapped difference in range is zero"
+                 (range-every zero? (range-map - r r)))
+    )
   )
- )
