65763fbe56ad496d9b491453b273653df047a493
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2021-04-13 22:51:44 -0400

    Use srfi-78 egg.

--- tests/srfi-196-test.scm
+++ tests/srfi-196-test.scm
@@ -22,39 +22,8 @@
 (import (scheme))
 (import (srfi 1))
 (import (only (srfi 133) vector-map))
+(import (srfi 78))
 (import (srfi 196))
-
-(define *tests-failed* 0)
-
-(define-syntax check
-  (syntax-rules (=>)
-    ((check expr => expected)
-     (if (equal? expr expected)
-       (begin
-         (display 'expr)
-         (display " => ")
-         (display expected)
-         (display " ; correct")
-         (newline))
-       (begin
-         (set! *tests-failed* (+ *tests-failed* 1))
-         (display "FAILED: for ")
-         (display 'expr)
-         (display " expected ")
-         (display expected)
-         (display " but got ")
-         (display expr)
-         (newline))))))
-
-(define (check-report)
-  (if (zero? *tests-failed*)
-      (begin
-       (display "All tests passed.")
-       (newline))
-      (begin
-       (display "TESTS FAILED: ")
-       (display *tests-failed*)
-       (newline))))
 
 (define (generator->list g)
   (let ((v (g)))
